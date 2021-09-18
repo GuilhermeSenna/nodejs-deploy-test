c1dd98973935f78d3fe35951fd36f6fa8a93328f
zBiggui <bigguisenna@gmail.com>
2021-09-18 15:36:28 -0300

    Teste com rota

--- app.js
+++ app.js
@@ -6,6 +6,10 @@ app.get('/', (req, res) => {
     res.send('<h1>My Node App </h1>');
 });
 
+app.get('/teste', (req, res) => {
+    res.send('<h1>teste de roteamento </h1>');
+});
+
 app.listen(5000, () => {
     console.log('App listening on port 5000!');
 });
